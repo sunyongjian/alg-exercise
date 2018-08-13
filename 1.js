beada6bf28a275068b145b99d77a35dfac89aa86
sunyongjian <sunyongjian@didichuxing.com>
2018-08-13 22:17:15 +0800

    feat: accum add

--- 1.js
+++ 1.js
@@ -5,6 +5,12 @@
 // accum("RqaEzty"); // "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 // accum("cwAt");    // "C-Ww-Aaa-Tttt"
 
+
+
+
+
+
+
 function accum(str) {
   return str.split('').reduce((res, cur, index) => `${res}-${cur.toUpperCase()}${cur.toLowerCase().repeat(index)}`, '').slice(1);
 }
@@ -12,3 +18,14 @@ function accum(str) {
 console.log(accum('abcd'));
 console.log(accum('RqaEzty'));
 console.log(accum('cwAt'));
+
+
+// 更优雅的方案
+
+function accum1(s) {
+  return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-')
+}
+
+console.log(accum1('abcd'));
+console.log(accum1('RqaEzty'));
+console.log(accum1('cwAt'));
