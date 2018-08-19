8fc72985bd114d535c12a705f3a9f75a5492629d
sunyongjian <704928775@qq.com>
2018-08-19 13:48:41 +0800

    fix: w1-7

--- week1/w1-7.js
+++ week1/w1-7.js
@@ -1,6 +1,6 @@
 
 const squareDigits = (num) => {
-  return Number(`${num}`.split('').map((item, index) => Math.pow(item, 2)).join(''));
+  return +`${num}`.split('').map(item => Math.pow(item, 2)).join('');
 };
 
 console.log(squareDigits(9119));
